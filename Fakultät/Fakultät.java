3132a5ad89d8f187d62e5f45d79ea6af1a261b44
Maxim Dehl <maxim.dehl@ohg-bensberg.de>
2022-11-16 12:42:31 +0100

    Fakultäten Projekt wurde hinzugefügt

---
+++ Fakultät/Fakultät.java
@@ -0,0 +1,48 @@
+import java.util.concurrent.TimeUnit;
+public class Fakultät
+{
+
+    public Fakultät() 
+    {
+        
+    }
+    
+    public int fakWhile(int n) 
+    {
+        int l = 1;
+        long startTime = System.nanoTime();
+        while(n > 0)
+        {
+            l = l * n;
+            n--;
+        }
+        long endTime = System.nanoTime();
+        long timeElapsed = endTime - startTime;
+        System.out.println("Execution time in nanoseconds: " + timeElapsed);
+        System.out.println("Execution time in milliseconds: " + timeElapsed / 1000000);
+        return l;
+    }
+    public int fakRekursivMitZeit(int n)
+    {
+        int o;
+        long startTime = System.nanoTime();
+        o = fakRekursiv(n);
+        long endTime = System.nanoTime();
+        long timeElapsed = endTime - startTime;
+        System.out.println("Execution time in nanoseconds: " + timeElapsed);
+        System.out.println("Execution time in milliseconds: " + timeElapsed / 1000000);
+        return 0;
+    }
+        
+    public int fakRekursiv(int n)
+    {
+        int l = 1;
+        if (n <= 1) 
+        {
+            return 1;
+        }else 
+        {
+            return fakRekursiv(n - 1) * n;
+        }
+    }
+}
