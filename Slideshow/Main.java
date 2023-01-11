bc3f75fc34c10701731c7bc1cfdd0332408a0f26
Maxim Dehl <maxim.dehl@ohg-bensberg.de>
2023-01-11 13:11:44 +0100

    Google Aufgabe

--- Slideshow/Main.java
+++ Slideshow/Main.java
@@ -1,34 +1,28 @@
 
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Main {
-
-    public static void main(String[] args) {
-        int interest;
-        //Liste von Photos einlesen in die ArrayList photos
-        ArrayList<Photo> photos = Photo.getPhotosFrom("d_pet_pictures.txt");
-        
-        
-        //Prüfung des interessierenden Faktors für das 1. und 2. Photo
-        System.out.println(photos.get(0).getInterestFactor(photos.get(1)));
-
-        //Wegschreiben einer ArrayList
-       
-        ArrayList<Photo> testausgabe = new ArrayList<Photo>();
-        for(int i = 0; i < 20; i++){
-            testausgabe.add(photos.get(i));
-        }
-
-        Photo.writePhotosToFile("output_beispiel.txt", testausgabe);
-
-         
-    }
-    
-    public static int hoechsteZahl()
+    ArrayList<Photo> photos = Photo.getPhotosFrom("d_pet_pictures.txt");
+    public void sortTags()
     {
-        for(int i = 0; i >= photos.length; i++)
+        for(int i = 0; i >= photos.size(); i++)
         {
-            return int;
+            if(photos.get(i).getNumberTags() < photos.get(i+1).getNumberTags())
+            {
+                swap(photos.get(i), photos.get(i+1));
+            }
         }
     }
+    public void swap(Photo A, Photo B)
+    {
+        int AIndex = photos.indexOf(A);
+        int BIndex = photos.indexOf(B);
+        photos.remove(A);
+        photos.add(AIndex, B);
+        photos.remove(B);
+        photos.add(BIndex, A);
+    }
+    //https://github.com/chameleonTK/hashcode-2019
 }
